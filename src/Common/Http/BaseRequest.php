bbbd7031c506fad71e2ce7042ec1689a1a435c6c
Julian Finkler <julian@billbee.io>
2024-07-30 13:40:35 +0200

    feat: Add config property to BaseRequest

--- src/Common/Http/BaseRequest.php
+++ src/Common/Http/BaseRequest.php
@@ -10,6 +10,11 @@ class BaseRequest
     #[Serializer\Type("string")]
     protected string $action = "";
 
+    /** @var array<string, string> */
+    #[Serializer\SerializedName("config")]
+    #[Serializer\Type("array<string, string>")]
+    protected array $config = [];
+
     public function getAction(): string
     {
         return $this->action;
@@ -18,5 +23,18 @@ class BaseRequest
     public function setAction(string $action): void
     {
         $this->action = $action;
+    }
+
+    /** @return array<string, string> */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    /** @param array<string, string> $config */
+    public function setConfig(array $config): BaseRequest
+    {
+        $this->config = $config;
+        return $this;
     }
 }
